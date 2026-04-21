1dc8088c69b7dff6f9e2feab0cec69a3ae22f5cf
Alex.Bachir <alex.bachir@laplateforme.io>
2026-04-21 10:16:02 +0200

    add spring initializr

---
+++ src/main/java/com/catchbook/CatchbookApplication.java
@@ -0,0 +1,13 @@
+package com.catchbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catchbook/CatchbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catchbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
