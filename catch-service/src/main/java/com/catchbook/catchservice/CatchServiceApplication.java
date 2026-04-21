eb292124452a5f840169236b65468920f61bcae0
Alex.Bachir <alex.bachir@laplateforme.io>
2026-04-21 11:23:38 +0200

    add micro service

---
+++ catch-service/src/main/java/com/catchbook/catchservice/CatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.catchbook.catchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchServiceApplication.class, args);
+	}
+
+}
